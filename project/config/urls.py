fefd893c3b5e62d9fbac984dbce7f0ddef2d5aab
Tamer <tamerinale@gmail.com>
2026-06-10 15:18:45 +0100

    Fix urls.py for Cloudinary

--- project/config/urls.py
+++ project/config/urls.py
@@ -18,10 +18,13 @@ from django.contrib import admin
 from django.urls import path , include
 from django.conf import settings
 from django.conf.urls.static import static
+import os
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app_project.urls')),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if not os.getenv('CLOUDINARY_CLOUD_NAME'):
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
